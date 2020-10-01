a1f70673bd310015d06425f3142451c697bd34e8
Charles Liu <liu.charles02@gmail.com>
2020-10-01 10:35:51 -0700

    added cpp

---
+++ cpp.cpp
@@ -0,0 +1,12 @@
+ void test(int n)
+ {
+  bool is_fizz = test_fizz(n);
+  bool is_buzz = test_buzz(n);
+  if (is_fizz || is_buzz) {
+	if (is_fizz) output_fizz();
+	if (is_buzz) output_buzz();
+  } else {
+	output_number(n);
+  }
+  output_end_of_line();
+ }
