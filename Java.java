d52b5c020ebdb3602b9e9e53e3c54af3f0b0b994
rahul <rsha256@berkeley.edu>
2020-10-01 14:12:54 -0400

    fix java

--- Java.java
+++ Java.java
@@ -1,15 +1,14 @@
 public class Java {
   public static void main(String[] args) {
-    String buzz =  "buzz", fizz = "fizz";
     for (int i = 1; i <= 100; i++) {
-      if (i % 15 == 0) {
-        System.out.println(buzz + fizz + " " + i));
-      } else if (i % 3  == 0 ) {
-        System.out.println(buzz + " " + i);
-      } else if (i % 5== 0) {
-        System.out.println(fizz + " "+ i);
-      }
-	  }
+      String s = "";
+      if (i % 3 == 0)
+        s += "Fizz";
+      if (i % 5 == 0)
+        s += "Buzz";
+      if (s.equals(""))
+        s = "" + i;
+      System.out.println(s); 
 	}
+  }
 }
-
