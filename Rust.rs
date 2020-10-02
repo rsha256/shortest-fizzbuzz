2efbac5651d5b8d910790fee98990c69b3d28045
meneza <51222153+meneza@users.noreply.github.com>
2020-10-01 18:59:29 -0700

    Make it readable

--- Rust.rs
+++ Rust.rs
@@ -1,1 +1,3 @@
-fn main() { (1..=100).for_each(|e| if e % 15 == 0 { println!("FizzBuzz") } else if e % 3 == 0 { println!("Fizz") } else if e % 5 == 0 { println!("Buzz") } else { println!("{}", e) }) }
+fn main() { 
+  (1..=100).for_each(|e| if e % 15 == 0 { println!("FizzBuzz") } else if e % 3 == 0 { println!("Fizz") } else if e % 5 == 0 { println!("Buzz") } else { println!("{}", e) })
+}
