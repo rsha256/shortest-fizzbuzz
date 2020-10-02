a9ccda194df033fe2031e0cd884e4f16ad0c262f
Oliver <42757516+carelesshippo@users.noreply.github.com>
2020-10-01 19:22:37 -0600

    Changed it to a very ugly one line conditional :)

--- Java.java
+++ Java.java
@@ -1,14 +1,7 @@
 public class Java {
-  public static void main(String[] args) {
-    for (int i = 1; i <= 100; i++) {
-      String s = "";
-      if (i % 3 == 0)
-        s += "Fizz";
-      if (i % 5 == 0)
-        s += "Buzz";
-      if (s.equals(""))
-        s = "" + i;
-      System.out.println(s); 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i % 3 == 0 && i % 5 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : String.valueOf(i))));
+        }
     }
-  }
 }
