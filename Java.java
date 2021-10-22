6ac7dab9df66f19dfac3f000cf6ec4978139116d
LukeStump <74683887+LukeStump@users.noreply.github.com>
2021-10-22 00:27:36 -0400

    One liner, for loop with no body
    i++<100; //combines i<100; and i++; (in that order). this lets us fit the code in the incrementor spot
    i%3<1 //equivalent to i%3==0
    i%3<1?"Fizz":"" //"Fizz" if i is divisible by 3, otherwise empty string
    i%5<1?"Buzz" //"Buzz" if i is divisible by 5, otherwise
    i%3>0?i:"" //i if i is not divisible by 3

--- Java.java
+++ Java.java
@@ -1,7 +1,5 @@
 public class Java {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(i % 3 == 0 && i % 5 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : String.valueOf(i))));
-        }
+        for(int i=0;i++<100;System.out.println((i%3<1?"Fizz":"")+(i%5<1?"Buzz":i%3>0?i:"")));
     }
 }
