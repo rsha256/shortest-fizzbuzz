31d9e114c0bdfb18d95dc6a1ef1cbde1512db4de
Rolf Martin Glomsrud <54860401+polsevev@users.noreply.github.com>
2023-11-12 20:09:46 +0100

    Shorter solution written in Rust (#48)
    * Shorter Rust solution
    
    * Update byte count of Rust solution

--- Rust.rs
+++ Rust.rs
@@ -1,3 +1,3 @@
-fn main() { 
-  (1..=100).for_each(|e| if e % 15 == 0 { println!("FizzBuzz") } else if e % 3 == 0 { println!("Fizz") } else if e % 5 == 0 { println!("Buzz") } else { println!("{}", e) })
+fn main() {
+  (1..=100).for_each(|x| println!("{}", match (x%3, x%5) {(0,0) => "FizzBuzz".into(), (0, _) => "Fizz".into(), (_,0) => "Buzz".into(), (_,_) => x.to_string()}))
 }
