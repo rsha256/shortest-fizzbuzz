430a961f60e4af3f809a0cdeb61471822c00cd55
Ben <benwirth@gmail.com>
2023-11-14 14:13:02 -0800

    Off by one fixed

--- Rust.rs
+++ Rust.rs
@@ -1,1 +1,1 @@
-fn main(){for i in 1..100{if i%3<1{print!("Fizz")}if i%5<1{print!("Buzz")}else if i%3>0{print!{"{i}"}}println!()}}
+fn main(){for i in 1..101{if i%3<1{print!("Fizz")}if i%5<1{print!("Buzz")}else if i%3>0{print!{"{i}"}}println!()}}
