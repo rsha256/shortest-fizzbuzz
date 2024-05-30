fcf7c92198a1d9ac63ca4db7bbdac36127a74786
Martín Durán <info@maduranma.com>
2024-05-30 14:56:17 +0200

    Added a shorter 69 bytes PHP version

--- PHP.php
+++ PHP.php
@@ -1,6 +1,2 @@
-foreach (range(1, 100) as $number) {
-    $isFizz = $number % 3 == 0;
-    $isBuzz = $number % 5 == 0;
-    $fizzBuzz = ($isFizz ? 'Fizz' : '') . ($isBuzz ? 'Buzz' : '');
-    echo  !empty($fizzBuzz) ? $fizzBuzz.'<br>' : (string)$number .'<br>';
-}
+<?php
+for(;@$i++<100;)echo@(['Fizz'][$i%3].['Buzz'][$i%5]?:$i),"\n";
