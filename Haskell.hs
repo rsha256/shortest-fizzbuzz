caf343a34459fd2940f6908e76a0fc6bcab7ee39
Lars Hollander <168178249+larshollander@users.noreply.github.com>
2024-05-31 21:05:49 +0200

    Shortened a bit

--- Haskell.hs
+++ Haskell.hs
@@ -1,6 +1,3 @@
-f n|(n`mod`3==0)="Fizz"++(b(1>0)n)
- |1>0=(b(1<0)n)
-b x n|n`mod`5==0="Buzz"
- |x=""
- |1>0=show n
+f n|n`mod`3==0="Fizz"++b(1>0)n|1>0=b(1<0)n
+b x n|n`mod`5==0="Buzz"|x=""|1>0=show n
 main=mapM(putStrLn.f)[1..100]
