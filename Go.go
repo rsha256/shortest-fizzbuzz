dd40000d4cebc3c3991705890e8f7f1bf55d2bca
Slava Krampetz <svl@dev4masses.com>
2024-10-10 14:26:15 +0700

    Go (#138)

--- Go.go
+++ Go.go
@@ -1,1 +1,19 @@
-package main;import f"fmt";func main(){p:=f.Print;for i:=1;i<101;i++{if i%3<1{p("Fizz")};if i%5<1{p("Buzz")};if i%3>0&&i%5>0{p(i)};p("\n")}}
+package main
+
+import . "fmt"
+
+func main() {
+	p := Print
+	for i := 1; i < 101; i++ {
+		if i%3 < 1 {
+			p("Fizz")
+		}
+		if i%5 < 1 {
+			p("Buzz")
+		}
+		if i%3 > 0 && i%5 > 0 {
+			p(i)
+		}
+		p("\n")
+	}
+}
